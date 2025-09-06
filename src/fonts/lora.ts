96aa36748c82b6c117d305f0c7f40c3c150dab9a
Abhijeet Nigam <basant.abhijeet2003@gmail.com>
2025-09-06 10:22:44 +0530

    Setup Next.js, Tailwind, TypeScript

---
+++ src/fonts/lora.ts
@@ -0,0 +1,8 @@
+import { Lora } from "next/font/google";
+
+export const lora = Lora({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-lora",
+  weight: ["400", "500", "600", "700"],
+});
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lora: ["var(--font-lora)", "serif"],
+        cursive: ['"Great Vibes"', "cursive"],
+      },
+      colors: {
+        brand: {
+          green: "#047857", // Tailwind green-700
+          black: "#1F2937", // Tailwind gray-800
+        },
+      },
+      animation: {
+        "gradient-x": "gradient-x 15s ease infinite",
+      },
+      keyframes: {
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+      textShadow: {
+        DEFAULT: "0 1px 3px rgba(0, 0, 0, 0.1)",
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const textShadows = theme("textShadow");
+      const utilities = {};
+
+      Object.entries(textShadows).forEach(([key, value]) => {
+        const className =
+          key === "DEFAULT" ? ".text-shadow" : `.text-shadow-${key}`;
+        utilities[className] = { textShadow: value };
+      });
+
+      addUtilities(utilities);
+    },
+  ],
+};
