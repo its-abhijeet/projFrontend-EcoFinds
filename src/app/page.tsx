84ef4097701a7fc473973bc1a02f554cf5fd6490
Abhijeet Nigam <basant.abhijeet2003@gmail.com>
2025-09-06 10:57:05 +0530

    initial arc/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello EcoFinds 👋</h1>
+    </main>
+  );
+}
